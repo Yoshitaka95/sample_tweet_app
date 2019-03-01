9e905febbece29564f5ff68298f9f618152cea08
Yoshitaka95 <harayo1000@yahoo.co.jp>
2019-03-01 05:58:55 +0000

    created rough structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,21 +4,43 @@ class PostsController < ApplicationController
   end
   
   def index
+    @posts = Post.all
   end
   
   def new
   end
   
   def create
+    @post = Post.new(content: params[:content])
+    
+    if  @post.save
+      redirect_to("/index")
+    else
+      render("/new")
+    end
+    
   end
   
   def edit_form
+    @post = Post.find_by(id: params[:id])
   end
   
   def edit
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    
+    if @post.save
+      redirect_to("/index")
+    else
+      render("/edit_form")
+    end
+    
   end
   
   def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    
   end
   
 end

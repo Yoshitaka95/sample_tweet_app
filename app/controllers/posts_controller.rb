58e4c813b404163dfc0167c11c45df038386deb2
Yoshitaka95 <harayo1000@yahoo.co.jp>
2019-03-01 04:42:58 +0000

    updated routes posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  
+  def home
+  end
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def edit_form
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/" => "posts#home"
+  get "index" => "posts#index"
+  get "new" => "posts#new"
+  get "edit/:id" => "posts#edit_form"
+  post "create" => "posts#create"
+  post "edit/:id" => "posts#edit"
+  post "destroy/:id" =>"posts#destroy"
 end
